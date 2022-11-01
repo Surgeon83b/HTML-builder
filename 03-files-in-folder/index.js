4d4ff6c27837e9f52cf65b2618758f56e181f8a4
Kanstantsin Piatkevich <surgeon-2@tut.by>
2022-11-01 03:28:07 +0300

    feat: complete 3rd task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,16 @@
+fs = require('fs');
+const path = require('path');
+
+const fileNames = fs.readdir(path.join(__dirname, 'secret-folder'), { withFileTypes: true },
+  (err, files) => {
+   // console.log(files);
+    files.forEach(file => {
+      if (file.isFile()) {
+       // console.log(file.name);
+        fs.stat(path.join(__dirname, 'secret-folder', file.name), (err, stats) => {
+          let ind = file.name.lastIndexOf('.');
+          console.log(`${file.name.substr(0, ind)} - ${file.name.substr(ind + 1)} - ${stats.size}`);
+        })
+      }
+    })
+  });
